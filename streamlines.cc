cb601c749eda8426c99c8a734917fb3a62abc6fc
Saad Jbabdi <saad@fmrib.ox.ac.uk>
2012-11-28 14:15:34 +0000

    fixed copyright

--- streamlines.cc
+++ streamlines.cc
@@ -1,71 +1,10 @@
 /*    Copyright (C) 2012 University of Oxford  */
 
-/*  Part of FSL - FMRIB's Software Library
-    http://www.fmrib.ox.ac.uk/fsl
-    fsl@fmrib.ox.ac.uk
-    
-    Developed at FMRIB (Oxford Centre for Functional Magnetic Resonance
-    Imaging of the Brain), Department of Clinical Neurology, Oxford
-    University, Oxford, UK
-    
-    
-    LICENCE
-    
-    FMRIB Software Library, Release 5.0 (c) 2012, The University of
-    Oxford (the "Software")
-    
-    The Software remains the property of the University of Oxford ("the
-    University").
-    
-    The Software is distributed "AS IS" under this Licence solely for
-    non-commercial use in the hope that it will be useful, but in order
-    that the University as a charitable foundation protects its assets for
-    the benefit of its educational and research purposes, the University
-    makes clear that no condition is made or to be implied, nor is any
-    warranty given or to be implied, as to the accuracy of the Software,
-    or that it will be suitable for any particular purpose or for use
-    under any specific conditions. Furthermore, the University disclaims
-    all responsibility for the use which is made of the Software. It
-    further disclaims any liability for the outcomes arising from using
-    the Software.
-    
-    The Licensee agrees to indemnify the University and hold the
-    University harmless from and against any and all claims, damages and
-    liabilities asserted by third parties (including claims for
-    negligence) which arise directly or indirectly from the use of the
-    Software or the sale of any products based on the Software.
-    
-    No part of the Software may be reproduced, modified, transmitted or
-    transferred in any form or by any means, electronic or mechanical,
-    without the express permission of the University. The permission of
-    the University is not required if the said reproduction, modification,
-    transmission or transference is done without financial return, the
-    conditions of this Licence are imposed upon the receiver of the
-    product, and all original and amended source code is included in any
-    transmitted product. You may be held legally responsible for any
-    copyright infringement that is caused or encouraged by your failure to
-    abide by these terms and conditions.
-    
-    You are not permitted under this Licence to use this Software
-    commercially. Use for which any financial return is received shall be
-    defined as commercial use, and includes (1) integration of all or part
-    of the source code or the Software into a product for sale or license
-    by or on behalf of Licensee to third parties or (2) use of the
-    Software or any derivative of it for research with the final aim of
-    developing software products for sale or license to a third party or
-    (3) use of the Software or any derivative of it for research with the
-    final aim of developing non-software products for sale or license to a
-    third party, or (4) use of the Software to provide any service to an
-    external organisation for which payment is received. If you are
-    interested in using the Software commercially, please contact Isis
-    Innovation Limited ("Isis"), the technology transfer company of the
-    University, to negotiate a licence. Contact details are:
-    innovation@isis.ox.ac.uk quoting reference DE/9564. */
+/*  CCOPYRIGHT */
+
 #include "streamlines.h"
 #include "warpfns/fnirt_file_reader.h"
 #include "warpfns/warpfns.h"
-
-#define MaxDimNIFTI 65536
 
 // Trajectory file writer
   int SpMat_HCP::SaveTrajFile(const string& basename)const
@@ -763,10 +702,6 @@ namespace TRACT{
 			 << roicind[i];
     
     applycoordchange(CoordMat1, m_stline.get_seeds().get_refvol().niftivox2newimagevox_mat().i());
-    //MISCMATHS::write_binary_matrix(CoordMat1,logger.appendDir("coords_for_fdt_matrix1"));
-    //if(CoordMat1.Nrows()<MaxDimNIFTI && CoordMat1.Ncols()<MaxDimNIFTI)
-    //write_matrix_as_volume(CoordMat1,logger.appendDir("coords_for_fdt_matrix1"));
-    //else
       write_ascii_matrix(CoordMat1,logger.appendDir("coords_for_fdt_matrix1"));
   }
   
@@ -803,9 +738,6 @@ namespace TRACT{
 	  }
 
     applycoordchange(CoordMat_tract2, m_lrmask.niftivox2newimagevox_mat().i());
-    //if(MAX(CoordMat_tract2.Nrows(),CoordMat_tract2.Ncols())<MaxDimNIFTI)
-    // write_matrix_as_volume(CoordMat_tract2,logger.appendDir("tract_space_coords_for_fdt_matrix2"));
-    //else
       write_ascii_matrix(CoordMat_tract2,logger.appendDir("tract_space_coords_for_fdt_matrix2"));
 
     save_volume(m_lookup2,logger.appendDir("lookup_tractspace_fdt_matrix2"));
@@ -828,10 +760,6 @@ namespace TRACT{
 			   << roicind[i];
       
       applycoordchange(CoordMat2, m_stline.get_seeds().get_refvol().niftivox2newimagevox_mat().i());
-      //MISCMATHS::write_ascii_matrix(CoordMat2,logger.appendDir("coords_for_fdt_matrix2.txt"));
-      //if(MAX(CoordMat2.Ncols(),CoordMat2.Nrows())<MaxDimNIFTI)
-      //write_matrix_as_volume(CoordMat2,logger.appendDir("coords_for_fdt_matrix2"));            
-      //else
 	write_ascii_matrix(CoordMat2,logger.appendDir("coords_for_fdt_matrix2"));            
 
     }
@@ -876,9 +804,6 @@ namespace TRACT{
     
     applycoordchange(mat, m_stline.get_seeds().get_refvol().niftivox2newimagevox_mat().i());
 
-    //if(MAX(mat.Nrows(),mat.Ncols())<MaxDimNIFTI)
-    //write_matrix_as_volume(mat,logger.appendDir("coords_for_fdt_matrix3"));
-    //else
       write_ascii_matrix(mat,logger.appendDir("coords_for_fdt_matrix3"));
 
     if(opts.lrmask3.value()!=""){
@@ -940,9 +865,6 @@ namespace TRACT{
 	  }
 
     applycoordchange(CoordMat_tract4, m_dtimask.niftivox2newimagevox_mat().i());
-    //if(MAX(CoordMat_tract4.Nrows(),CoordMat_tract4.Ncols())<MaxDimNIFTI)
-    //write_matrix_as_volume(CoordMat_tract4,logger.appendDir("tract_space_coords_for_fdt_matrix4"));
-    //else
       write_ascii_matrix(CoordMat_tract4,logger.appendDir("tract_space_coords_for_fdt_matrix4"));
     save_volume(m_lookup4,logger.appendDir("lookup_tractspace_fdt_matrix4"));
 
@@ -964,9 +886,6 @@ namespace TRACT{
     
     applycoordchange(CoordMat4, m_stline.get_seeds().get_refvol().niftivox2newimagevox_mat().i());
 
-    //if(MAX(CoordMat4.Nrows(),CoordMat4.Ncols())<MaxDimNIFTI)
-    //write_matrix_as_volume(CoordMat4,logger.appendDir("coords_for_fdt_matrix4"));                    
-    //else
       write_ascii_matrix(CoordMat4,logger.appendDir("coords_for_fdt_matrix4"));                    
   }
   
