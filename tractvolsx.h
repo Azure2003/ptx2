891799b6226353792c30eab128298538e15727c5
Moises Fernandez <moisesf@fmrib.ox.ac.uk>
2014-08-03 17:36:41 +0000

    Correct small bug (precision) when generating random numbers for uniform distribution

--- tractvolsx.h
+++ tractvolsx.h
@@ -77,7 +77,7 @@ namespace TRACTVOLSX{
 	  return 0;
 	}
 	else{
-	  float rtmp=(float)rand()/(float(RAND_MAX)+1) * float(fibvec.size());
+	  double rtmp=(rand()/(double(RAND_MAX)+1)) * fibvec.size();
 	  return (fibvec[ (int)floor(rtmp) ]);
 	}
       }
@@ -87,7 +87,8 @@ namespace TRACTVOLSX{
 	  return 0;
 	}
 	if(mode==3){//sample all
-	  return int(floor((float)(nfibres)*(float)rand()/(float(RAND_MAX)+1)));
+	  double rtmp=(rand()/(double(RAND_MAX)+1)) * nfibres;
+	  return int(floor(rtmp));
 	}
 	else{
 	  if(mode==1){//sample all>thresh
@@ -102,7 +103,7 @@ namespace TRACTVOLSX{
 	      return 0;
 	    }
 	    else{
-	      float rtmp=(float)rand()/(float(RAND_MAX)+1) * float(fibvec.size());
+	      double rtmp=(rand()/(double(RAND_MAX)+1)) * fibvec.size();
 	      return (fibvec[ (int)floor(rtmp) ]);
 	    }
 	  }
